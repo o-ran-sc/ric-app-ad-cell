fac6e3c6f76ff6a86932516a2219a9efef525ff8
Sunil N <sunil.n@samsung.com>
2024-10-09 08:31:16 +0530

    Add function calculateNewScore in Scaler.py
    Change-Id: Ib1e21e5cbe222b674e8d2ed79976574d8b79cb34
    Signed-off-by: Sunil N <sunil.n@samsung.com>

--- src/manager/Scaler.py
+++ src/manager/Scaler.py
@@ -10,7 +10,12 @@ log = Util.setup_logger()
 class Scaler:
 
     def calculateNewScore(self, cell_name, kpi_list):
-        pass
+        df_cell_score = dict()
+        df_cell_score['Short name'] = cell_name
+        for kpi in kpi_list:
+            df_cell_score[kpi] = 0
+        df_cell_score['is_new_cell'] = 1
+        return pd.DataFrame([df_cell_score])
 
     def calculateExistingScore(self, cell, kpi_list, df_kpi_value):
         pass
